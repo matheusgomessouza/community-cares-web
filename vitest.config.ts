5db709844f890c9fd139b6bc6cfe485c0b445795
-m <-m>
2025-10-07 20:41:04 -0300

    fix: Fxing version mismatch between the Vite instance used by Vitest and the vite-plugin-tsconfig-paths plugin

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,19 @@
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from 'vite-plugin-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
   test: {
     environment: "jsdom",
     globals: true, // Optional: Allows global access to `expect`, `vi`, etc.
     setupFiles: "./vitest.setup.ts", // Add the setup file to load jest-dom
     typecheck: {
       tsconfig: "./tsconfig.json",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      // Add other aliases from your tsconfig.json paths here
     },
   },
 });
